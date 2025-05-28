b15e7a32ec3c3456675d9ed0092589bcc9e2202c
Marakas <igorpetriv.bsc@gmail.com>
2025-05-29 00:58:29 +0300

    first try

---
+++ src/components/MainHeder/MainHeder.tsx
@@ -0,0 +1,13 @@
+import css from "./MainHeder.module.css";
+
+export default function MainHeder() {
+  return (
+    <div className={css.header}>
+      <h1>
+        Not sure what to watch?
+        <br /> Start exploring.
+        <br /> Let the perfect movie find you.
+      </h1>
+    </div>
+  );
+}
